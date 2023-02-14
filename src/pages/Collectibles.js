1932af8e1b4c1e24de74e982840bc853ac706f07
Johanna Fransson <adrina0002@gmail.com>
2023-02-14 11:03:00 +0000

    remove old code

--- src/pages/Collectibles.js
+++ src/pages/Collectibles.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FetchNftsForm } from "../components/Collectibles/FetchNftsForm";
 import { NftImages } from "../components/Collectibles/NftImages";
 import "../index.css";
+<<<<<<< Updated upstream:src/pages/Collectibles.js
 
 export default function Collectibles({ route }) {
   /*   let location = useLocation();
@@ -15,13 +16,30 @@ export default function Collectibles({ route }) {
     }
     fetchData();
   }, [searchInput]); */
+=======
+import { setupSDK } from "../setupSDK";
+
+export default function Collectibles() {
+  setupSDK();
+  const [nfts, setNfts] = useState([]);
+  const fetchNfts = async (address: string, chainId: string) => {
+    const nfts: any = await NftService.getNfts(address, +chainId);
+    console.log(JSON.stringify(nfts));
+    setNfts(nfts);
+  };
+>>>>>>> Stashed changes:src/pages/Collectibles.tsx
 
   return (
     <div className="align-middle text-center text-banner mb-24">
       <div style={{ padding: 20 }} className="h-screen flex ">
         <div className="grow space-y-10 self-start">
+<<<<<<< Updated upstream:src/pages/Collectibles.js
           <FetchNftsForm />
           <NftImages />
+=======
+          <FetchNftsForm onSubmit={fetchNfts} />
+          <NftImages nfts={nfts} />
+>>>>>>> Stashed changes:src/pages/Collectibles.tsx
         </div>
       </div>
     </div>
