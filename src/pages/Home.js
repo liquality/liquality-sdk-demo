a51cd0d81ff7383c57b224a7db19172bc745b361
Johanna Fransson <adrina0002@gmail.com>
2023-02-23 10:59:26 +0000

    sw debug and test, but not working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -89,7 +89,7 @@ export default function Home() {
             <a href="https://www.youtube.com/channel/UCEXqBmzqPa_KeWtgu0g9KGQ">
               <img
                 className="h-11 rounded-lg"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png"
+                src="https://seeklogo.com/images/V/vimeo-icon-logo-441934AEB1-seeklogo.com.png"
                 alt="image description"
               />
             </a>
