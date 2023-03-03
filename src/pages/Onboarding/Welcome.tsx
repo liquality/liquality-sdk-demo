e80e5b59f1311313123bbbfb18c318077ab9a53d
Donia Amer <doniawamer@Donias.local>
2023-03-02 23:04:22 -0500

    url

--- src/pages/Onboarding/Welcome.tsx
+++ src/pages/Onboarding/Welcome.tsx
@@ -37,7 +37,7 @@ const verifierMap: Record<string, any> = {
 
 // 1. Setup Service Provider
 const directParams = {
-  baseUrl: `https://demo.liquality.io/serviceworker`,
+  baseUrl: `https://inquisitive-dusk-bbe67f.netlify.app/serviceworker`,
   enableLogging: true,
   networkUrl: "https://goerli.infura.io/v3/a8684b771e9e4997a567bbd7189e0b27",
   network: "testnet" as any,
